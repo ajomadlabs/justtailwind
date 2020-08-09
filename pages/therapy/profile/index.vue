0933e01190b59250c406b59b1b4a493e251346ba
Ajo John <ajojohn555@gmail.com>
2020-08-09 21:45:03 +0530

    :boom: [therpay] - implemented top navbar

---
+++ pages/therapy/profile/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- MAIN SECTION -->
+        <!-- TOP NAVBAR COMPONENT -->
+        <TopNavbar/>
+        <!-- END -->
+        <!-- END -->
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        TopNavbar: () => import("@/components/therapy/TopNavbar/index.vue")
+    },
+    data () {
+        return {}
+    }
+}
+</script>
